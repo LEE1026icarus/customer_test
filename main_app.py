1921f40962d920811204d84e7223dbc4d776c15c
LEESUNHYUNG <113161569+LEE1026icarus@users.noreply.github.com>
2024-10-15 19:59:45 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -186,7 +186,7 @@ def history_page():
 
 # Streamlit 시작
 if __name__ == "__main__":
-    st.set_page_config(layout="wide", page_title="AI 고객제표 분석",theme="light" )
+    st.set_page_config(layout="wide", page_title="AI 고객제표 분석")
     local_css("style.css")  # CSS 파일 로드
     
     if st.session_state['logged_in']:
